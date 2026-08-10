453b90007b2f732c0e432ac584370b5a2216163b
trgrnw <109393749+trgrnw@users.noreply.github.com>
2026-08-10 15:12:11 +0300

    Configure app for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/context/AuthContext";
 import { I18nProvider } from "@/i18n/I18nContext";
 import Index from "./pages/Index";
@@ -18,7 +18,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <I18nProvider>
-        <BrowserRouter>
+        <HashRouter>
           <AuthProvider>
             <Routes>
               <Route path="/" element={<Index />} />
@@ -27,7 +27,7 @@ const App = () => (
               <Route path="*" element={<NotFound />} />
             </Routes>
           </AuthProvider>
-        </BrowserRouter>
+        </HashRouter>
       </I18nProvider>
     </TooltipProvider>
   </QueryClientProvider>
